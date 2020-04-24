cabe2cc4389f08eb60d022226e0383195576815c
Jas <c.jasminher@gmail.com>
2020-04-24 18:20:59 -0400

    style: Added styling to theme the whole app in dark mode

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import About from './pages/About';
-import Resume from './pages/Resume';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import { Route, Switch} from 'react-router-dom';
-import { BrowserRouter} from 'react-router-dom';
-
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/";
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
+
   return (
     <BrowserRouter>
-    <div className="App">
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Navbar />
 
-     <Navbar/>
-
-     <Switch>
-       <Route exact path="/" component={About} />
-       <Route path="/resume" component={Resume}/>
-       <Route path="/projects" component={Projects} />
-       <Route path="/contact" component={Contact} />
-     </Switch>
-     
-    </div>
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route path="/resume" component={Resume} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
