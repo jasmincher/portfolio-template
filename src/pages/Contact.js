90bcc3e622d8068520e96806e5c5d35b358f7bbb
Jas <c.jasminher@gmail.com>
2020-04-25 17:24:10 -0400

    feat: Created contact page to handle form submissions and validating each field

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,167 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import '../css/Contact.css';
 
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
 
-function Contact(){
+const isFormValid = ({ errors, ...inputs }) => {
+  let valid = true;
+
+  //makes sure form isn't empty
+  Object.values(errors).forEach((value) => {
+    value.length > 0 && (valid = false);
+  });
+
+  //makes sure the form is filled out
+  Object.values(inputs).forEach((value) => {
+    value === "" && (valid = false);
+  });
+
+  return valid;
+};
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: [null, null],
+      name: "",
+      email: "",
+      message: "",
+      errors: {
+        name: "",
+        email: "",
+        message: "",
+      },
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const inputs = [this.state.name, this.state.email, this.state.message];
+
+    if (isFormValid(this.state)) {
+      e.target.reset();
+      this.setState({ success: ["Email Sent", true] });
+      console.log("Form submission successful!");
+
+      //if form fields are filled in correctly then we submit form
+
+      axios
+        .post(
+          "https://smartforms.dev/submit/5ea39d998a02626591cb5aa0",
+          inputs,
+          {
+            headers: { Accept: "multipart/form-data" },
+          }
+        )
+        .then(function (res) {
+          console.log(res);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    } else {
+      this.setState({ success: ["Fill in all fields", false] });
+      console.error("Form submission unsuccessful");
+    }
+  };
+
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let errors = { ...this.state.errors };
+    console.log(errors);
+
+    switch (name) {
+      case "name":
+        errors.name = value.length < 2 ? "Name is too short" : "";
+        break;
+      case "email":
+        errors.email = emailRegex.test(value) ? "" : "Invalid email";
+        break;
+      case "message":
+        errors.message = value.length < 6 ? "Message must be longer" : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value, success: [null, null] });
+  };
+
+  render() {
+    const { errors } = this.state;
+
     return (
-        <div>
-            This is contact page
-        </div>
-    )
+      <div className="form-container">
+
+
+
+
+
+     <div>{this.state.success[0]}</div>
+
+
+        <form onSubmit={this.handleSubmit} className="mail-form" noValidate>
+          
+          
+          <div id="item1">
+
+          <label>Name</label>
+          <input
+            className={errors.name.length > 0 ? "error" : null}
+            type="text"
+            name="name"
+            noValidate
+            onChange={this.handleChange}
+            />
+            </div>
+
+            <div id="item2">
+
+         
+          <label>Email</label>
+          <input
+            className={errors.email.length > 0 ? "error" : null}
+            type="email"
+            name="email"
+            noValidate
+            onChange={this.handleChange}
+            />
+            </div>
+
+        <div id="item3">
+
+
+          <label>Message</label>
+          <textarea
+            className={
+                errors.message.length > 0
+                ? "error"
+                : null
+            }
+            type="textarea"
+            name="message"
+            noValidate
+            onChange={this.handleChange}
+            ></textarea>
+
+            </div>
+        
+
+          <input type="hidden" name="_gotcha"></input>
+          <button type="submit" className="form-btn">
+            Submit
+          </button>
+        </form>
+      </div>
+
+      
+    );
+  }
 }
 
 export default Contact;
