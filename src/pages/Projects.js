7bc3f901f630f1e1d2f6478ee67fa86442426f0c
Jas <c.jasminher@gmail.com>
2020-04-24 21:16:36 -0400

    feat: Added blurb components into projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import Blurb from "../components/Blurb";
+import project from "../assets/images/project.jpg";
+
+const text =
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
 
-function Projects(){
-    return (
-        <div>
-            This is Projects page
-        </div>
-    )
+function Projects() {
+  return (
+    <div>
+      <Blurb image={project} text={text} textPosition="left" />
+      <Blurb image={project} text={text} textPosition="right" />
+      <Blurb image={project} text={text} textPosition="left" />
+
+
+    </div>
+  );
 }
 
 export default Projects;
