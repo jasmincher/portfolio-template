7a5d8e3bb767c3aae08ec3da962724c97eec1259
Jas <c.jasminher@gmail.com>
2020-04-24 20:42:54 -0400

    feat: Added divs to contain about info, heading and image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from "react";
+import avatar from "../assets/images/avatar.jpg";
+import "../css/About.css";
 
+function About() {
+  return (
+    <div className="about-container">
+      <div className="about-heading">
+        <h1>About Me</h1>
+      </div>
+      
+      <img src={avatar} className="profile-img" />
 
-function About(){
-    return (
-        <div>
-            This is About page
-        </div>
-    )
+      <div className="about-info">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Suspendisse interdum consectetur libero id faucibus nisl tincidunt
+          eget nullam. Lorem dolor sed viverra ipsum nunc aliquet bibendum. Eget
+          gravida cum sociis natoque penatibus. Ipsum a arcu cursus vitae. Mi
+          eget mauris pharetra et ultrices neque ornare. At augue eget arcu
+          dictum varius duis at. Ac tortor dignissim convallis aenean. Purus sit
+          amet luctus venenatis lectus magna fringilla urna porttitor.
+        </p>
+
+        <p>
+          Convallis tellus id interdum velit laoreet id donec ultrices
+          tincidunt. Nulla facilisi nullam vehicula ipsum a arcu cursus.
+          Tincidunt praesent semper feugiat nibh. Urna condimentum mattis
+          pellentesque id nibh tortor. Neque laoreet suspendisse interdum
+          consectetur libero id. Suscipit tellus mauris a diam maecenas sed enim
+          ut. Ultricies leo integer malesuada nunc. Vitae semper quis lectus
+          nulla at volutpat.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
