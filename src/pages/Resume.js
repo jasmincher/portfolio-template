74f9047604ccd47a1ae2dfe86132803a4f085560
Jas <c.jasminher@gmail.com>
2020-04-27 00:02:37 -0400

    feat: Added iframe to load pdf

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import '../css/Resume.css';
+import Header from '../components/Header';
 
 
 function Resume(){
     return (
-        <div>
-            This is Resume page
+        <div className="resume-container">
+           
+           <Header heading="My Resume" subheading="Contact me if anything stands out"/>
+           <iframe src="" className="resume-pdf"></iframe>
+           
         </div>
     )
 }
 
 export default Resume;
+
+
+
+
