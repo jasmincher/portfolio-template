01450619e3bb401ed8aa355acac99d31dd8bd9a2
Jas <c.jasminher@gmail.com>
2020-04-27 20:51:42 -0400

    style/refactor: Added react-pdf component to display resume. Added conditions to change state based on window width resulting to changing state, which updates the width of the pdf.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,21 +1,64 @@
-import React from 'react';
-import '../css/Resume.css';
-import Header from '../components/Header';
+import React from "react";
+import "../css/Resume.css";
+import Header from "../components/Header";
+import resumePDF from "../assets/Resume.pdf";
+import { pdfjs } from "react-pdf";
+import { Document, Page } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+class Resume extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageNumber: 1,
+      resize: null,
+    };
+  }
 
-function Resume(){
+  componentDidMount() {
+    this.resizeScreen();
+    window.addEventListener("resize", this.resizeScreen.bind(this));
+  }
+
+  resizeScreen() {
+    let width = window.innerWidth;  //width of window 
+
+    if (width >= 1200) { 
+      this.setState({ resize: 1000 });
+    } else if (width >= 992) {
+      this.setState({ resize: 882 });
+    } else if (width >= 668) {
+      this.setState({ resize: 558 });
+    } else if (width >= 480) {
+      this.setState({ resize: 380 });
+    } else {
+      this.setState({ resize: 300 });
+    }
+  }
+
+  render() {
+    const { pageNumber } = this.state;
+
     return (
-        <div className="resume-container">
-           
-           <Header heading="My Resume" subheading="Contact me if anything stands out"/>
-           <iframe src="" className="resume-pdf"></iframe>
-           
-        </div>
-    )
+      <div className="resume-container">
+        <Header
+          heading="My Resume"
+          subheading="Contact me if anything stands out"
+        />
+        <Document
+          file={resumePDF}
+          className="resume-pdf"
+          loading="Loading Resume..."
+        >
+          <Page
+            pageNumber={pageNumber}
+            width={this.state.resize}
+            className="pdf-page"
+            renderAnnotationLayer={false}
+          />
+        </Document>
+      </div>
+    );
+  }
 }
-
 export default Resume;
-
-
-
-
