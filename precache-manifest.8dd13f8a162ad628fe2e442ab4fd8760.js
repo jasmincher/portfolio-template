48ae57f1324defd40e78330d59164775b7c0c312
Stephen <stephen_s@Stephens-MacBook-Pro.local>
2020-04-27 23:23:11 -0400

    Updates

---
+++ precache-manifest.8dd13f8a162ad628fe2e442ab4fd8760.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4d2fcf911e2e54c4078669dcf65853",
+    "url": "/stephen-portfolio/index.html"
+  },
+  {
+    "revision": "db07680ad0d17bd5b7ea",
+    "url": "/stephen-portfolio/static/css/main.3e3ceff2.chunk.css"
+  },
+  {
+    "revision": "7cb14975748b2d7f6969",
+    "url": "/stephen-portfolio/static/js/2.5603fb87.chunk.js"
+  },
+  {
+    "revision": "6734d0bfe367c40a5b2a23ab2f00b933",
+    "url": "/stephen-portfolio/static/js/2.5603fb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db07680ad0d17bd5b7ea",
+    "url": "/stephen-portfolio/static/js/main.7b9b5b42.chunk.js"
+  },
+  {
+    "revision": "fd479662accb3957627e",
+    "url": "/stephen-portfolio/static/js/runtime-main.1f77382a.js"
+  },
+  {
+    "revision": "0d2bc2039651571252e89244e64e49dc",
+    "url": "/stephen-portfolio/static/media/Resume.0d2bc203.pdf"
+  },
+  {
+    "revision": "8f2fdea5410d4c384fdbb812c3ce18a8",
+    "url": "/stephen-portfolio/static/media/stephen.8f2fdea5.jpeg"
+  },
+  {
+    "revision": "7cd96fdcc202b66bda2ca0d8497ef425",
+    "url": "/stephen-portfolio/static/media/stephen2.7cd96fdc.jpeg"
+  },
+  {
+    "revision": "e8fd34fe8a180fd6ec62e4aa1209a647",
+    "url": "/stephen-portfolio/static/media/stephen3.e8fd34fe.jpeg"
+  }
+]);
